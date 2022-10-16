220fc14d4d299bbd1a952075d5aac523db636d98
prasert73 <115928283+prasert73@users.noreply.github.com>
2022-10-16 22:43:36 +0700

    minor adjustment

--- ole.js
+++ ole.js
@@ -1,13 +1,11 @@
 
 let data;
 let response;
-let randomIndex;
+//let randomIndex;
 
-window.onload = init;
-function init () {
-    fetchQuote();
-}
+window.onload = fetchData();
 
+/*
 async function fetchQuote() {
     response = await fetch("https://type.fit/api/quotes");
     data = await response.json();  
@@ -20,7 +18,28 @@ async function fetchQuote() {
     twtLink = twtLink +" - " +data[randomIndex].author.replace(/\s/g, '%20')
     document.getElementById('tweet-quote').href = twtLink;
     };
+*/
+
+//let myData=[{ text: "Genius is one percent inspiration and ninety-nine percent perspiration.", author: "Thomas Edison" },  { text: "You can observe a lot just by watching.", author: "Yogi Berra" }, { text: "A house divided against itself cannot stand.", author: "Abraham Lincoln" }, { text: "Difficulties increase the nearer we get to the goal.", author: "Johann Wolfgang von Goethe" }]
 
 
+async function fetchData() {
+    response = await fetch("https://type.fit/api/quotes");
+    data = await response.json(); 
+    getNewQuote();
+    return data;
+}
 
 
+function getNewQuote(){
+    let randomIndex = Math.floor(Math.random() * data.length);
+    let randomQuote = data[randomIndex].text;
+    let randomauthor = data[randomIndex].author;
+    document.getElementById('text').innerText = randomQuote;
+    document.getElementById('author').innerText = randomauthor;
+    let twtLink = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=';
+    let twtMsg = data[randomIndex].text.replace(/\s/g,"%20");
+    twtLink += twtMsg.replace(/^(.+)$/g, '"$1"');
+    twtLink = twtLink +" - " +data[randomIndex].author.replace(/\s/g, '%20')
+    document.getElementById('tweet-quote').href = twtLink;
+}
